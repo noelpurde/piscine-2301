19ed0eb780265f4fc19dd66679558178c17611f3
noelpurde <noelpurde@gmail.com>
2023-06-19 14:16:40 +0200

    C02

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[i] == '\0' )
+	{
+		return (1);
+	}
+	while (str[i] != '\0')
+	{
+		if ((str[i] >= 'A' && str [i] <= 'Z')
+			|| (str [i] >= 'a' && str [i] <= 'z'))
+			i++;
+		else
+			return (0);
+	}	
+	return (1);
+}
+// int	main()
+// {
+// 	printf("%d", ft_str_is_alpha("ckvlxm"));
+// 	printf("\n%d", ft_str_is_alpha("sdf32dg"));
+// }
---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 48 && str [i] <= 57))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+// int	main()
+// {
+// 	printf("%d", ft_str_is_numeric("493487395"));
+// 	printf("\n%d", ft_str_is_numeric("sdf32dg"));
+// }
